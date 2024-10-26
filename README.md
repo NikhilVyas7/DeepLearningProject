36d159f5ce8decf64325c62a61737e5c1dd6e809
Ankith Naveen Thalanki <athalanki3@login-ice-1.pace.gatech.edu>
2024-10-25 21:56:58 -0400

    Finished Dataset and Dataloader

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ### Pace Setup
 
-First, login into PACE-ICE via `ssh <GT_USERNAME>@login-ice.pace.gatech.edu`. It will ask you for a password, which will just be your gt password. For example, for me it would be `ssh athalanki3@login-ice.pace.gatech.edu`
+First, login into PACE-ICE via `ssh <GT_USERNAME>@login-ice.pace.gatech.edu`. It will ask you for a password, which will just be your gt password. For example, for me it would be `ssh athalanki3@login-ice.pace.gatech.edu`. Then you want to allocate youself an interactive compute node with a gpu via `salloc -N1 -t6:00:00 --gres=gpu:1 --cpus-per-task=15`. The `-t6:00:00` specifies you want 6 hours of time. The excessive number of cpus cores really helps when loading data.
 
 ### Repo Setup
 
@@ -19,6 +19,16 @@ The dataset is located at this [dropbox](https://www.dropbox.com/scl/fo/k33qdif1
 dataset is located in the repo with the name `FloodNet`. To unzip, run 
 `UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE unzip FloodNet.zip -d FloodNet`. The unzip disable zipbomb is required because the unzip function falsely
 thinks the dataset is a zipbomb.
+
+
+## Task List
+
+
+- Create Dataset and Dataloader class for this dataset (Done)
+
+- Create script to convert label image containign (1s,2s.) etc to something that can be visualized with colors ,according to `FloodNet/ColorMasks-FloodNetv1.0/ColorPalette-Values.xlsx`.
+
+- Look into using a multi-processing Array to speed up the DataSet by taking the data from memory after the first epoch
 
 
 
--- data/datasets.py
+++ data/datasets.py
@@ -1,1 +1,92 @@
 #Define dataset class as well as dataloader class
+import cv2
+import torch
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import time
+import os
+from PIL import Image
+class FloodDataset(Dataset):
+    #This class is the generic FloodDataset with seperate transforms for image and label
+    def __init__(self,image_dir,label_dir,transform=None,target_transform=None):
+        
+        self.image_dir = image_dir
+        self.label_dir = label_dir
+
+        for filepath in os.listdir(image_dir):
+            #Validate label exists
+            img_num = filepath.split(".")[0]# 7926.png -> 7926
+            label = img_num + "_lab.png" #All labels are pngs, while images are jpgs.
+            label_path = os.path.join(self.label_dir,label)
+            if not os.path.exists(label_path):
+                raise Exception(f"Label {label} doesn't exist for {img_num}")
+            
+        self.images = os.listdir(image_dir)
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def __len__(self):
+        return len(self.images)
+        
+
+    def __getitem__(self,idx):
+        image = self.images[idx]
+        img_num = image.split(".")[0]# 7926.png -> 7926
+        label = img_num + "_lab.png" #All labels are pngs, while images are jpgs.
+        label_path = os.path.join(self.label_dir,label)
+        image_path = os.path.join(self.image_dir,image)
+
+        #Load images via cv2, and resize them (Some of them are slightly larger)
+        loaded_image = cv2.imread(image_path)
+        #Switch color channels
+        loaded_image = cv2.cvtColor(loaded_image,cv2.COLOR_BGR2RGB)
+        loaded_image = cv2.resize(loaded_image, (3000,4000))
+
+        loaded_label = cv2.imread(label_path)
+        loaded_label = cv2.resize(loaded_label, (3000,4000), interpolation=cv2.INTER_NEAREST) #Because labels must be specific values
+
+        if self.transform:
+            loaded_image = self.transform(loaded_image)
+        if self.target_transform:
+            loaded_label = self.target_transform(loaded_label)
+        return loaded_image, loaded_label
+
+class SharedTransformFloodDataset(FloodDataset):
+    #This class has another parameter, shared_transforms, which are applied between the image and the label, and applied
+    #after other transforms
+    def __init__(self,image_dir,label_dir,transform=None,target_transform=None,shared_transform=None):
+        super(SharedTransformFloodDataset,self).__init__(image_dir,label_dir,transform,target_transform)
+        self.shared_transform = shared_transform
+    def __getitem__(self,idx):
+        image, label = super(SharedTransformFloodDataset,self).__getitem__(idx)
+        if self.shared_transform:
+            seed = np.random.randint(1,1000)
+            torch.manual_seed(seed)
+            image = self.shared_transform(image)
+            torch.manual_seed(seed)
+            label = self.shared_transform(label)
+        return image, label
+
+        
+if __name__ == "__main__":
+    print("Starting...")
+
+
+
+    shared_transforms = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    image_dir = "/home/hice1/athalanki3/scratch/DeepLearningProject/FloodNet/FloodNet-Supervised_v1.0/test/test-org-img"
+    label_dir = "/home/hice1/athalanki3/scratch/DeepLearningProject/FloodNet/FloodNet-Supervised_v1.0/test/test-label-img"
+    test_dataset = SharedTransformFloodDataset(image_dir,label_dir,shared_transform=shared_transforms)
+    
+
+    test_dataloader = DataLoader(test_dataset,batch_size=10,shuffle=False,num_workers=8,pin_memory=True)
+    start_time = time.time()
+    imgs, labels = next(iter(test_dataloader))
+    end_time = time.time()
+    #Img sizes are all 3000,4000.
+    print(imgs.shape)
+    print(labels.shape)
+    print(f"Time: {end_time - start_time}")
