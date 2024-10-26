499d50fabe50b27d21e27416857bbb3471f05206
Ankith Naveen Thalanki <athalanki3@atl1-1-03-010-30-0.pace.gatech.edu>
2024-10-25 22:40:59 -0400

    Added cacheing in later epochs by using a multi-processing Array shared by all workers

--- data/datasets.py
+++ data/datasets.py
@@ -1,4 +1,6 @@
-#Define dataset class as well as dataloader class
+#Define dataset class
+import multiprocessing as mp
+import ctypes
 import cv2
 import torch
 from torchvision import transforms, datasets
@@ -9,11 +11,13 @@ import os
 from PIL import Image
 class FloodDataset(Dataset):
     #This class is the generic FloodDataset with seperate transforms for image and label
+    #Cacheing before transforms for now
     def __init__(self,image_dir,label_dir,transform=None,target_transform=None):
         
         self.image_dir = image_dir
         self.label_dir = label_dir
 
+        #Validate all labels exist for every image
         for filepath in os.listdir(image_dir):
             #Validate label exists
             img_num = filepath.split(".")[0]# 7926.png -> 7926
@@ -25,27 +29,49 @@ class FloodDataset(Dataset):
         self.images = os.listdir(image_dir)
         self.transform = transform
         self.target_transform = target_transform
-    
+        self.use_cache = False
+        #Set up cache for images
+        self.shared_array_base_images = mp.Array(ctypes.c_uint8, len(self.images) * 3 * 4000 * 3000)
+        shared_array_images = np.ctypeslib.as_array(self.shared_array_base_images.get_obj())
+        self.shared_array_images =shared_array_images.reshape(len(self.images),4000,3000,3)
+
+        #Set up cache for labels
+        self.shared_array_base_labels = mp.Array(ctypes.c_uint8, len(self.images) * 4000 * 3000)
+        shared_array_labels = np.ctypeslib.as_array(self.shared_array_base_labels.get_obj())
+        self.shared_array_labels = shared_array_labels.reshape(len(self.images),4000,3000)
+
     def __len__(self):
         return len(self.images)
         
-
+    def set_use_cache(self,use_cache):
+        self.use_cache = use_cache
     def __getitem__(self,idx):
-        image = self.images[idx]
-        img_num = image.split(".")[0]# 7926.png -> 7926
-        label = img_num + "_lab.png" #All labels are pngs, while images are jpgs.
-        label_path = os.path.join(self.label_dir,label)
-        image_path = os.path.join(self.image_dir,image)
+        if not self.use_cache:
+            image = self.images[idx]
+            img_num = image.split(".")[0]# 7926.png -> 7926
+            label = img_num + "_lab.png" #All labels are pngs, while images are jpgs.
+            label_path = os.path.join(self.label_dir,label)
+            image_path = os.path.join(self.image_dir,image)
 
-        #Load images via cv2, and resize them (Some of them are slightly larger)
-        loaded_image = cv2.imread(image_path)
-        #Switch color channels
-        loaded_image = cv2.cvtColor(loaded_image,cv2.COLOR_BGR2RGB)
-        loaded_image = cv2.resize(loaded_image, (3000,4000))
+            #Load images via cv2, and resize them (Some of them are slightly larger)
+            loaded_image = cv2.imread(image_path)
+            #Switch color channels
+            loaded_image = cv2.cvtColor(loaded_image,cv2.COLOR_BGR2RGB)
+            loaded_image = cv2.resize(loaded_image, (3000,4000))
 
-        loaded_label = cv2.imread(label_path)
-        loaded_label = cv2.resize(loaded_label, (3000,4000), interpolation=cv2.INTER_NEAREST) #Because labels must be specific values
+            loaded_label = cv2.imread(label_path,cv2.IMREAD_GRAYSCALE)
+            
+            loaded_label = cv2.resize(loaded_label, (3000,4000), interpolation=cv2.INTER_NEAREST) #Because labels must be specific values
 
+            #Store label and image into cache
+            self.shared_array_images[idx] = loaded_image
+            self.shared_array_labels[idx] = loaded_label
+        else:
+            loaded_label = self.shared_array_labels[idx]
+            loaded_image = self.shared_array_images[idx]
+
+        #Resize label to have a color channel
+        loaded_label = loaded_label[...,np.newaxis]
         if self.transform:
             loaded_image = self.transform(loaded_image)
         if self.target_transform:
@@ -90,3 +116,16 @@ if __name__ == "__main__":
     print(imgs.shape)
     print(labels.shape)
     print(f"Time: {end_time - start_time}")
+
+    print("Using cache....")
+    test_dataloader.dataset.set_use_cache(True)
+
+    start_time = time.time()
+    imgs, labels = next(iter(test_dataloader))
+    end_time = time.time()
+    #Img sizes are all 3000,4000.
+    print(imgs.shape)
+    print(labels.shape)
+    print(f"Time: {end_time - start_time}")
+
+    #Might want to consider cacheing after the transform idk, can deal with that later
